a5e73a8fa22bb3df864d4b1e3a6c5efa05be2f64
Vadim Ignatov <ignatov0131@gmail.com>
2024-05-20 14:20:23 +0300

    feat: update loader

--- app/components/ui/loader/index.vue
+++ app/components/ui/loader/index.vue
@@ -7,23 +7,21 @@
 
 <script lang="ts" setup>
 interface Props {
-  size?: "s" | "m" | "l"
+  size?: "s" | "m" | "l",
+  theme?: "primary" | "secondary"
 }
 const props = withDefaults(defineProps<Props>(), {
-  size: "m"
+  size: "m",
+  theme: "primary"
 });
 
 const $style = useCssModule();
 const classes = computed(() => ([
-  $style.uiLoader, $style[`size--${props.size}`]
+  $style[`size--${props.size}`], $style[`theme--${props.theme}`]
 ]));
 </script>
 
 <style module>
-.uiLoader {
-  color: var(--color-primary-1);
-}
-
 .size--s {
   width: 18px;
   height: 18px;
@@ -37,5 +35,13 @@ const classes = computed(() => ([
 .size--l {
   width: 32px;
   height: 32px;
+}
+
+.theme--primary {
+  color: var(--color-primary-1);
+}
+
+.theme--secondary {
+  color: var(--color-secondary-1);
 }
 </style>
