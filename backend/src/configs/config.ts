4bade4630c4cb6f632802f07b339370d05433028
Vadim Ignatov <ignatov0131@gmail.com>
2024-05-23 13:31:34 +0300

    feat: create config file

---
+++ backend/src/configs/config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  PORT: z.string().default("8000"),
+  JWT_SECRET: z.string(),
+  JWT_ACCESS_EXPIRATION_MINUTES: z.string().default("60"),
+  JWT_REFRESH_EXPIRATION_DAYS: z.string().default("30"),
+});
+
+const env = EnvSchema.parse(process.env);
+
+export default {
+  port: env.PORT,
+  jwt: {
+    secret: env.JWT_SECRET,
+    accessExpirationMinutes: env.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: env.JWT_REFRESH_EXPIRATION_DAYS,
+  }
+}
