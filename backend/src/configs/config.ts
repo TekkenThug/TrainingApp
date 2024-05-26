e3d7834643f52854f30e87b5c4f033ccb002dbde
Vadim Ignatov <ignatov0131@gmail.com>
2024-05-26 12:42:30 +0300

    feat: create logger

--- backend/src/configs/config.ts
+++ backend/src/configs/config.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   APP_WHITELIST: z.string(),
+  NODE_ENV: z.string().regex(/^prod|dev$/gm),
   PORT: z.string().default("8000"),
   JWT_SECRET: z.string(),
   JWT_ACCESS_EXPIRATION_MINUTES: z.string().default("30"),
@@ -13,6 +14,7 @@ const env = EnvSchema.parse(process.env);
 
 export default {
   appWhitelist: env.APP_WHITELIST.split(";"),
+  env: env.NODE_ENV,
   port: env.PORT,
   jwt: {
     secret: env.JWT_SECRET,
