ee8bbaecbdeae87c8331fa3b941342a43a8424b6
Vadim Ignatov <ignatov0131@gmail.com>
2024-05-29 16:12:21 +0300

    feat: create validation middleware

---
+++ backend/src/middlewares/validate.ts
@@ -0,0 +1,17 @@
+import { ZodSchema } from "zod";
+import { fromError } from "zod-validation-error";
+import pick from "lodash.pick";
+import status from "statuses";
+import { NextFunction, Request } from "express";
+import { ApiError } from "@/utils/errors";
+
+export default (schema: ZodSchema) => (req: Request, _: Response, next: NextFunction) => {
+  const object = pick(req, ["params", "query", "body"]);
+  const result = schema.safeParse(object);
+
+  if (!result.success) {
+    return next(new ApiError(status("Bad request"), fromError(result.error).toString()));
+  }
+
+  return next();
+};
