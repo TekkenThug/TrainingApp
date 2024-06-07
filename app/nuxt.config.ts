79386fbb315bc813dd9073edfe207372b8a651ab
Vadim Ignatov <ignatov0131@gmail.com>
2024-06-07 14:27:48 +0300

    feat: update nuxt config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -10,7 +10,8 @@ export default defineNuxtConfig({
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com" },
-        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Viga&display=swap" }
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Viga&display=swap" },
+        { rel: "manifest", href: "/site.webmanifest" }
       ]
     }
   },
@@ -25,5 +26,7 @@ export default defineNuxtConfig({
     "normalize.css",
     "reset-css",
     "~/assets/styles/global.css"
-  ]
+  ],
+
+  pwa: {}
 })
