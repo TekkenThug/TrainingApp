a6881dddffcb035b4dcd43b77f8ec8b8da278b7a
Vadim Ignatov <ignatov0131@gmail.com>
2024-06-18 16:04:39 +0300

    feat: add as

--- backend/src/controllers/books.ts
+++ backend/src/controllers/books.ts
@@ -2,7 +2,7 @@ import { catchAsync } from "@/utils/errors";
 import BookService from "@/services/BookService";
 
 const get = catchAsync(async (req, res) => {
-  res.send(await BookService.get(req.query.search));
+  res.send(await BookService.get(req.query.search as string));
 });
 
 export default { get };
